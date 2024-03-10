a29d624198525bc1feadbded6d54f16fcd59bbb6
Takeru KIDO <keidy2463@gmail.com>
2024-03-10 23:36:33 +0900

    Add AZURE_OPENAI_API_DEPLOYMENT_NAME

--- aoai.py
+++ aoai.py
@@ -1,5 +1,6 @@
 """Run ReAct with Azure OpenAI"""
 
+import os
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor, create_react_agent
@@ -9,19 +10,12 @@ from langchain_openai import AzureChatOpenAI
 
 load_dotenv()
 
-
 llm = AzureChatOpenAI(
     openai_api_version="2024-02-15-preview",
-    azure_deployment="gpt-4",
+    azure_deployment=os.getenv("AZURE_OPENAI_API_DEPLOYMENT_NAME"),
 )
-# ツールを定義
-wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
-tools = [wikipedia]
-# ReAct用のプロンプトを用意（0-shotのReAct用のプロンプトです。）
+tools = [WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())]
 prompt = hub.pull("hwchase17/react")
-# Agentを作成
 agent = create_react_agent(llm, tools, prompt)
-# AgentExecutorを作成
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-# 処理開始
 agent_executor.invoke({"input": "2023年に開催したWorld Baseball Classicの優勝国は？"})
