a35243040687cd0ec82c6ec752cbe4eb268036db
Takeru KIDO <keidy2463@gmail.com>
2024-03-13 21:12:04 +0900

    Change from wikipedia to google search

--- by_azure_openai.py
+++ by_azure_openai.py
@@ -1,11 +1,11 @@
-"""Run ReAct with Azure OpenAI"""
+"""Run ReAct with Azure OpenAI and """
 
 import os
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor, create_react_agent
-from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
-from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
+from langchain_community.tools.google_search.tool import GoogleSearchRun
+from langchain_community.utilities.google_search import GoogleSearchAPIWrapper
 from langchain_openai import AzureChatOpenAI
 
 load_dotenv()
@@ -14,7 +14,7 @@ llm = AzureChatOpenAI(
     openai_api_version="2024-02-15-preview",
     azure_deployment=os.getenv("AZURE_OPENAI_API_DEPLOYMENT_NAME"),
 )
-tools = [WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())]
+tools = [GoogleSearchRun(api_wrapper=GoogleSearchAPIWrapper())]
 prompt = hub.pull("hwchase17/react")
 agent = create_react_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
