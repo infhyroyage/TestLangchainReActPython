e449642423989608d155dd0f5bf884349d2239c7
Takeru KIDO <keidy2463@gmail.com>
2024-03-13 21:20:35 +0900

    Add Groq API

---
+++ by_groq.py
@@ -0,0 +1,17 @@
+"""Run ReAct with Groq"""
+
+from dotenv import load_dotenv
+from langchain import hub
+from langchain.agents import AgentExecutor, create_react_agent
+from langchain_community.tools.google_search.tool import GoogleSearchRun
+from langchain_community.utilities.google_search import GoogleSearchAPIWrapper
+from langchain_groq import ChatGroq
+
+load_dotenv()
+
+llm = ChatGroq(model_name="mixtral-8x7b-32768")
+tools = [GoogleSearchRun(api_wrapper=GoogleSearchAPIWrapper())]
+prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+agent_executor.invoke({"input": "2023年に開催したWorld Baseball Classicの優勝国は？"})
