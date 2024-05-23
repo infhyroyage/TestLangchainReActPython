bc529ea3830e9de341e7e80af454c6ee2fe569af
Takeru KIDO <keidy2463@gmail.com>
2024-05-23 11:34:33 +0900

    Update by_azure_openai.py

--- by_azure_openai.py
+++ by_azure_openai.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor, create_react_agent
+from langchain_core.messages import HumanMessage
 from langchain_google_community import GoogleSearchAPIWrapper, GoogleSearchRun
 from langchain_openai import AzureChatOpenAI
 
@@ -18,6 +19,19 @@ agent = create_react_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(
     agent=agent, tools=tools, handle_parsing_errors=True, verbose=True
 )
+chat_history = []
+
 with open("input.txt", "r", encoding="utf-8") as file:
     input_text = file.read().replace("\n", "\\n")
-    agent_executor.invoke({"input": input_text})
+
+    while True:
+        result = agent_executor.invoke(
+            {"input": input_text, "chat_history": chat_history}
+        )
+        chat_history.extend([HumanMessage(content=input_text), result["output"]])
+
+        try:
+            input_text = input("Enter additional input (or press Ctrl+C to exit): ")
+        except KeyboardInterrupt:
+            print("\nTerminated by user.")
+            break
