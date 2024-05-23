3059bb7182afc4fe9b4394d022b23f20d2f773c3
Takeru KIDO <kido.takeru@fujitsu.com>
2024-05-23 14:14:16 +0900

    Fix using chat history

--- by_azure_openai.py
+++ by_azure_openai.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 from langchain import hub
 from langchain.agents import AgentExecutor, create_react_agent
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_google_community import GoogleSearchAPIWrapper, GoogleSearchRun
 from langchain_openai import AzureChatOpenAI
 
@@ -14,7 +14,7 @@ llm = AzureChatOpenAI(
     azure_deployment=os.getenv("AZURE_OPENAI_API_DEPLOYMENT_NAME"),
 )
 tools = [GoogleSearchRun(api_wrapper=GoogleSearchAPIWrapper())]
-prompt = hub.pull("hwchase17/react")
+prompt = hub.pull("hwchase17/react-chat")
 agent = create_react_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(
     agent=agent, tools=tools, handle_parsing_errors=True, verbose=True
@@ -28,7 +28,9 @@ with open("input.txt", "r", encoding="utf-8") as file:
         result = agent_executor.invoke(
             {"input": input_text, "chat_history": chat_history}
         )
-        chat_history.extend([HumanMessage(content=input_text), result["output"]])
+        chat_history.extend(
+            [HumanMessage(content=input_text), AIMessage(content=result["output"])]
+        )
 
         try:
             input_text = input("Enter additional input (or press Ctrl+C to exit): ")
